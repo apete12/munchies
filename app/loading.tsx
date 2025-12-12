c2403401ac88a14b4134502dcb6f295bccf1d871
Alex Peterson <aspeterson.126@gmail.com>
2025-12-11 22:31:52 -0600

    Remove link from loading comp

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link';
-
 export default function Loading() {
   return (
     <div className={'flex flex-col'}>
       <h2 className={'text-center mb-4 text-2xl text-black'}>
         {'Loading your restaurants!'}
       </h2>
-      <Link href={'/'} className={'hover:underline text-green-900 font-bold'}>
-        Head back home!
-      </Link>
     </div>
   );
 }
